b360c04cbb563c4e428fd3ea4f4116f2856cb1e9
Winston Robson <43570913+gumdropsteve@users.noreply.github.com>
2020-12-06 01:14:11 -0800

    Create ggplot_qplot.r

---
+++ day_01/ggplot_qplot.r
@@ -0,0 +1,13 @@
+library(ggplot2)
+
+roll <- function(die, p){
+  dice <- sample(die, 2, replace=TRUE, prob=p)
+  dice
+}
+
+die <- 1:6
+probability <- c(1/6, 1/6, 1/6, 1/6, 1/6, 1/6)
+
+rolls <- replicate(1000000, roll(die, probability))
+                   
+qplot(rolls)
