1fbc0d158cd9a009741b90924be506712f720a26
Winston Robson <43570913+gumdropsteve@users.noreply.github.com>
2020-12-08 01:03:26 -0800

    Update 03_while_loops.r

--- day_03/03_while_loops.r
+++ day_03/03_while_loops.r
@@ -13,3 +13,25 @@ while (count < v){
   print(count)
   count = count + 1
 }
+
+
+x <- 10
+count <- 0
+values <- rep(NA, x)
+
+while (count < x){
+  print(values)
+  count = count + 1
+  values[count] <- count
+}
+
+
+x <- 10
+i <- 0
+values <- rep(NA, x)
+
+while (i < x){
+  print(values)
+  i = i + 1
+  values[i] <- i
+}
