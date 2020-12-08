5df3491a8cb60c1f21d347b02a5c26c0fa13378b
Winston Robson <43570913+gumdropsteve@users.noreply.github.com>
2020-12-07 23:41:16 -0800

    Create 03_while_loop.r

---
+++ day_03/03_while_loop.r
@@ -0,0 +1,15 @@
+count <- 0
+
+while (count < 10){
+  print(count)
+  count = count + 1
+}
+
+
+v <- 9  # same thing but using a variable instead of retyping a digit
+count <- 0
+
+while (count < v){
+  print(count)
+  count = count + 1
+}
