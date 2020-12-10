adcbd56ccfaadda102dc9e7338bdb3be70a14dec
Mikio Harman <49764112+mpHarm88@users.noreply.github.com>
2020-12-10 01:08:55 -0800

    add day_05 (#5)
    Co-authored-by: mpHarm88 <mpHarm88@users.noreply.github.com>

---
+++ day_05/movie_theater_project.r
@@ -0,0 +1,42 @@
+# Potential Questions to Answer:
+# 1. Create snacks that the customers can buy and randomize who buys which snack
+# 2. Pretend you own multiple theaters and run two simulations to represent each theater and plot the results
+# 3. Create conditional statements for movies that may be PG-13 and children are not allowed to watch
+
+# Cost for adults and children
+ticket_cost <- ? 
+ticket_cost_child <- ? 
+movies <- c('', '', '', '', '')  # List 5 of your favorite movies
+screens <-  # How many screens does the theater have? (assume 1 per movie)
+seats <-  # How many seats does each theater hold
+week_days <- rep(0, 7)  # Store totals for each day
+
+# iterate through the week
+for () {
+  
+  # Keep track of total revenue for the day
+
+  
+  # iterate through the amount of screens on a particular day
+  for () {
+    
+    # Calculate  how many adults and children are watching the movie
+    visitors_adults <- sample(, 1)
+    visitors_children <- sample((),1)
+    
+    # Calculate the revenue for adults and children
+    
+    
+    # Calculate revenue, and add to running total for the day
+    
+    
+  }
+  # Save total to the corresponding day
+  
+}
+
+# Make a barchart showing total revenue per day
+  
+# Make any other chart
+
+# Which day had the highest revenue?
