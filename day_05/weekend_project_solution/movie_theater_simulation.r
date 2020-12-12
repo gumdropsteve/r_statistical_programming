eaad142c7ecdd871f2185f4762fe505eda0d5415
Winston Robson <43570913+gumdropsteve@users.noreply.github.com>
2020-12-12 06:00:05 -0800

    functionize
    movie_sim <- function() >> shift + down arrow key >> tab >> shift + {

--- day_05/weekend_project_solution/movie_theater_simulation.r
+++ day_05/weekend_project_solution/movie_theater_simulation.r
@@ -9,7 +9,7 @@ ticket_cost_child <- 8
 
 # set up snacks, snack prices, and odds any customer purchases each
 snacks <- c(NA, 'popcorn','small_candy', 'big_candy')
-snack_prices <- c(0, 9.75, 4.25, 7)
+snack_prices <- c(0, 9.75, 4.25, 7.25)
 snack_odds <- c(0.5, 0.34, 0.05, 0.11)
 
 # list 5 of your favorite movies or shows
@@ -28,44 +28,50 @@ seats <- 50
 # make a record book to store total revenue for each day
 week_days <- rep(0, 7)
 
-# iterate through each day of the week
-for (day in 1:length(week_days)) {
   
-  # keep track of total revenue for this day
-  this_days_revenue <- 0
+theater_sim <- function(){
+  # iterate through each day of the week
+  for (day in 1:length(week_days)) {
+    
+    # keep track of total revenue for this day
+    this_days_revenue <- 0
+    
+    # iterate through the each of your screens for this day
+    for (s in 1:screens) {
+      
+      # calculate how many adults and how many children are watching this screen
+      visitors_adults <- sample(seats, 1)
+      visitors_children <- sample((seats - visitors_adults), 1)
+      
+      # calculate this screen's revenue for adults, and for children
+      revenue_adults <- visitors_adults * ticket_cost
+      revenue_children <- visitors_children * ticket_cost_child
+      
+      # find snack choices (represented by their index value) for everyone watching
+      total_visitors <- visitors_adults + visitors_children
+      snack_choice_indexes <- sample(1:length(snacks),
+                                     total_visitors, 
+                                     replace=TRUE,
+                                     prob=snack_odds)
+      # use the snack choice indexes to find which snack & the sum of their prices
+      snack_choices <- snacks[snack_choice_indexes]  
+      revenue_snacks <- sum(snack_prices[snack_choice_indexes])
   
-  # iterate through the each of your screens for this day
-  for (s in 1:screens) {
-    
-    # calculate how many adults and how many children are watching this screen
-    visitors_adults <- sample(seats, 1)
-    visitors_children <- sample((seats - visitors_adults), 1)
-    
-    # calculate this screen's revenue for adults, and for children
-    revenue_adults <- visitors_adults * ticket_cost
-    revenue_children <- visitors_children * ticket_cost_child
-    
-    # find snack choices (represented by their index value) for everyone watching
-    total_visitors <- visitors_adults + visitors_children
-    snack_choice_indexes <- sample(1:length(snacks),
-                                   total_visitors, 
-                                   replace=TRUE,
-                                   prob=snack_odds)
-    # use the snack choice indexes to find which snack & the sum of their prices
-    snack_choices <- snacks[snack_choice_indexes]  
-    revenue_snacks <- sum(snack_prices[snack_choice_indexes])
-
-    # calculate this screen's total revenue, and add to running total for the day
-    this_screens_ticket_revenue <- revenue_adults + revenue_children
-    this_screens_revenue_for_today <- this_screens_ticket_revenue + revenue_snacks
-    this_days_revenue <- this_days_revenue + this_screens_revenue_for_today
+      # calculate this screen's total revenue, and add to running total for the day
+      this_screens_ticket_revenue <- revenue_adults + revenue_children
+      this_screens_revenue_for_today <- this_screens_ticket_revenue + revenue_snacks
+      this_days_revenue <- this_days_revenue + this_screens_revenue_for_today
+    }
+    # write today's total revenue at it's index in the record book
+    week_days[day] <- this_days_revenue
   }
-  # write today's total revenue at it's index in the record book
-  week_days[day] <- this_days_revenue
+  week_days
 }
 
+theater_01 <- theater_sim()
+
 # make a barchart showing total revenue per day
-barplot(week_days,  # <- what's being plotted
+barplot(theater_01,  # <- what's being plotted
         main = "Total Movie Revenue Per Day",  # chart title
         xlab = "Day of Week",  # x axis label
         ylab = "Revenue (USD)",  # y axis label
@@ -75,9 +81,9 @@ barplot(week_days,  # <- what's being plotted
 )
 
 # make any other chart
-plot(week_days)  # simple scatter plot
+plot(theater_01)  # simple scatter plot
 
 # which day had the highest revenue?
-index_of_max_value <- which.max(week_days)  # <- asks, which index has the max value?
+index_of_max_value <- which.max(theater_01)  # <- asks, which index has the max value?
 days_of_the_week <- c("Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat")
 days_of_the_week[index_of_max_value]
