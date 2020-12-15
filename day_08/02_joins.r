1460a710f02b59bae7edf0f87e314dfecc7d9f25
Winston Robson <43570913+gumdropsteve@users.noreply.github.com>
2020-12-14 23:35:39 -0800

    Create 02_joins.r

---
+++ day_08/02_joins.r
@@ -0,0 +1,28 @@
+# https://r4ds.had.co.nz/relational-data.html#understanding-joins
+
+x <- tribble(
+  ~key, ~val_x,
+  1, "x1",
+  2, "x2",
+  3, "x3"
+)
+y <- tribble(
+  ~key, ~val_y,
+  1, "y1",
+  2, "y2",
+  4, "y3"
+)
+
+x %>% 
+  inner_join(y, by = "key")
+
+x %>% 
+  left_join(y, by = "key")
+
+x %>% 
+  right_join(y, by = "key")
+
+x %>% 
+  full_join(y, by = "key")
+
+# more: https://github.com/gumdropsteve/datasets/tree/master/zillow
